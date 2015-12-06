f1f6c0bb05174a842524dfb172a3018e49b86497
Matthias Stumpp <mstumpp@gmail.com>
2015-12-06 16:37:13 +0100

    Merge branch 'master' into triplet_loss

---
+++ src/caffe/layers/max_margin_loss_layer.cpp
@@ -0,0 +1,114 @@
+#include "caffe/loss_layers.hpp"
+
+namespace caffe
+{
+	template <typename Dtype>
+	void MaxMarginLossLayer<Dtype>::LayerSetUp(
+		const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+		LossLayer<Dtype>::LayerSetUp(bottom, top);
+		CHECK_EQ(bottom[0]->channels(), bottom[1]->channels());
+		CHECK_EQ(bottom[0]->height(), 1);
+		CHECK_EQ(bottom[0]->width(), 1);
+		CHECK_EQ(bottom[1]->height(), 1);
+		CHECK_EQ(bottom[1]->width(), 1);
+		CHECK_EQ(bottom[2]->channels(), 1);
+		CHECK_EQ(bottom[2]->height(), 1);
+		CHECK_EQ(bottom[2]->width(), 1);
+		diff_.Reshape(bottom[0]->num(), bottom[0]->channels(), 1, 1);
+		diff_sq_.Reshape(bottom[0]->num(), bottom[0]->channels(), 1, 1);
+		dist_sq_.Reshape(bottom[0]->num(), 1, 1, 1);
+		// vector of ones used to sum along channels
+		summer_vec_.Reshape(bottom[0]->channels(), 1, 1, 1);
+		for (int i = 0; i < bottom[0]->channels(); ++i)
+			summer_vec_.mutable_cpu_data()[i] = Dtype(1);
+		b = this->layer_param_.max_margin_loss_param().b();
+		c = this->layer_param_.max_margin_loss_param().c();
+	}
+
+	template <typename Dtype>
+	void MaxMarginLossLayer<Dtype>::Forward_cpu(
+		const vector<Blob<Dtype>*>& bottom,
+		const vector<Blob<Dtype>*>& top) {
+		int count = bottom[0]->count();
+		caffe_sub(
+			count,
+			bottom[0]->cpu_data(),  // a
+			bottom[1]->cpu_data(),  // b
+			diff_.mutable_cpu_data());  // a_i-b_i
+		const int channels = bottom[0]->channels();
+		Dtype margin = this->layer_param_.max_margin_loss_param().margin();
+		Dtype loss(0.0);
+		for (int i = 0; i < bottom[0]->num(); ++i) {
+			dist_sq_.mutable_cpu_data()[i] = caffe_cpu_dot(channels,
+				diff_.cpu_data() + (i*channels), diff_.cpu_data() + (i*channels));
+			Dtype yij;
+			if (static_cast<int>(bottom[2]->cpu_data()[i])) {  // similar pairs
+				yij = 1.0;
+			}
+			else {  // dissimilar pairs
+				yij = -1.0;
+			}
+			loss += std::max(margin - yij * (b - dist_sq_.cpu_data()[i]), Dtype(0.0));
+		}
+		// average
+		loss = loss / static_cast<Dtype>(bottom[0]->num());
+		top[0]->mutable_cpu_data()[0] = loss;
+	}
+
+	template <typename Dtype>
+	void MaxMarginLossLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
+		const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+		Dtype margin = this->layer_param_.max_margin_loss_param().margin();
+		int changed = 0;
+		int unchanged = 0;
+		int similar = 0;
+		Dtype simAvg = 0;
+		Dtype dissimAvg = 0;
+		int dissimilar = 0;
+		for (int i = 0; i < 2; ++i) {
+			if (propagate_down[i]) {
+				const Dtype sign = (i == 0) ? 1 : -1;
+				const Dtype alpha = sign * top[0]->cpu_diff()[0] /
+					static_cast<Dtype>(bottom[i]->num());
+				int num = bottom[i]->num();
+				int channels = bottom[i]->channels();
+				for (int j = 0; j < num; ++j) {
+					Dtype* bout = bottom[i]->mutable_cpu_diff();
+					Dtype yij;
+					if (static_cast<int>(bottom[2]->cpu_data()[j])) {  // similar pairs
+						yij = 1;
+						similar++;
+						simAvg += dist_sq_.cpu_data()[j];
+					}
+					else {  // dissimilar pairs
+						yij = -1;
+						dissimilar++;
+						dissimAvg += dist_sq_.cpu_data()[j];
+					}
+					if (yij * (b - dist_sq_.cpu_data()[j]) <= margin) {
+						Dtype scalar = alpha * (2.0) * yij;
+						b += alpha * yij * c;
+						caffe_cpu_axpby(
+							channels,
+							scalar,
+							diff_.cpu_data() + (j*channels),
+							Dtype(0.0),
+							bout + (j*channels));
+						changed++;
+					}
+					else {
+						caffe_set(channels, Dtype(0), bout + (j*channels));
+						unchanged++;
+					}
+				}
+			}
+		}
+		std::cout << "Changed: " << changed << ", Unchanged: " << unchanged << ", Similar: " << similar
+				<< ", Dissimilar: " << dissimilar << " B: " << b <<std::endl;
+		std::cout << "SimAvg: " << simAvg / (Dtype) bottom[0]->num() << " Dissim: "
+				<< dissimAvg / (Dtype) bottom[0]->num() << std::endl;
+	}
+
+	INSTANTIATE_CLASS(MaxMarginLossLayer);
+	REGISTER_LAYER_CLASS(MaxMarginLoss);
+}
